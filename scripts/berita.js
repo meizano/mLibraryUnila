576cfaec36506d9c53693fec389dcdd0818b7fc3
Mudipra <mudipra24@gmail.com>
2019-03-07 15:30:57 +0700

    Add files via upload

---
+++ scripts/berita.js
@@ -0,0 +1,39 @@
+// Berita
+let hasilberita = document.getElementById('hasilberita');
+
+fetchberita();
+
+// Mengambil data JSON dari server
+function fetchberita() {
+    var url = 'http://localhost/mLIbraryUnilaUpdate/'; // URL RSS
+    fetch(url)
+        .then((resp) => resp.html())
+        .then(function (data) {
+            hasilberita.classList.remove("alert", "alert-warning", "alert-success");
+            console.log(data);
+            hasilberita.innerHTML = 'data masuk';
+            // hasilberita.innerHTML = `
+            // Hasil: ${data['info']}
+            // <br/>
+            // `;
+            
+            // for (let i = 0; i < data['data'].length; i++) {
+            //     hasilberita.innerHTML += `
+            //     <span>${i+1}</span>
+            //     <a target="_blank" href="${data['data'][i][4]}" >${data['data'][i][0]}</a>
+            //     <span>${data['data'][i][1]}</span>
+            //     <span>${data['data'][i][2]}</span>
+            //     <span>${data['data'][i][3]}</span>
+            //     <br/>
+            //     `;
+            // }
+
+            // hasilberita.classList.add("alert", "alert-success");
+            
+        })
+        .catch(function (error) {
+            hasilberita.classList.remove("alert", "alert-warning",  "alert-success");
+            hasilberita.innerHTML = "Masalah: " + JSON.stringify(error);
+            hasilberita.classList.add("alert", "alert-warning");
+        });
+};
---
+++ scripts/cariuniuca.js
@@ -0,0 +1,42 @@
+// Cari Union Catalog
+let cariUniUca = document.getElementById('cariUniUca');
+let keywordUniUca = document.querySelector('form#cariUniUca [name="keywords"]');
+let hasilUniUca  = document.getElementById('hasilUniUca');
+
+cariUniUca.addEventListener('click', function () {
+   fetchUniUca(keywordUniUca.value);
+});
+
+// Mengambil data JSON dari server
+function fetchUniUca(kata) {
+    var url = './dispatcher/UniUcajson.php?keywords=' + kata; // URL dari data JSON
+    fetch(url)
+        .then((resp) => resp.json())
+        .then(function (data) {
+            hasilUniUca.classList.remove("alert", "alert-warning", "alert-success");
+            
+            hasilUniUca.innerHTML = `
+            Hasil: ${data['info']}
+            <br/>
+            `;
+            
+            for (let i = 0; i < data['data'].length; i++) {
+                hasilUniUca.innerHTML += `
+                <span>${i+1}</span>
+                <a target="_blank" href="${data['data'][i][4]}" >${data['data'][i][0]}</a>
+                <span>${data['data'][i][1]}</span>
+                <span>${data['data'][i][2]}</span>
+                <span>${data['data'][i][3]}</span>
+                <br/>
+                `;
+            }
+
+            hasilUniUca.classList.add("alert", "alert-success");
+            
+        })
+        .catch(function (error) {
+            hasilUniUca.classList.remove("alert", "alert-warning",  "alert-success");
+            hasilUniUca.innerHTML = JSON.stringify(error);
+            hasilUniUca.classList.add("alert", "alert-warning");
+        });
+};
