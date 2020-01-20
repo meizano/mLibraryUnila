ed188ebd214dc7f6c8ee0e6f7a03ad7171979b76
Mudipra <mudipra24@gmail.com>
2020-01-21 03:13:56 +0700

    Update dbconfig.php

--- dispatcher/dbconfig.php
+++ dispatcher/dbconfig.php
@@ -1,9 +1,16 @@
 <?php
     # dbconfig.php
 
-    # konfigurasi database
-    $host = 'localhost';
-    $dbname = 'perpus';
-    $username = 'root';
-    $password = '';
+//     # konfigurasi database localhost
+//     $host = 'localhost';
+//     $dbname = 'perpus';
+//     $username = 'root';
+//     $password = '';
+
+    # konfigurasi database elib
+    $host = '10.10.100.6';
+    $dbname = 'elib';
+    $username = 'aldi';
+    $password = 'aldi2019sql';
+
 ?>
