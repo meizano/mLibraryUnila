b827e3cf6b316ecffe200dd7b2f67dd3966dd263
Mudipra <mudipra24@gmail.com>
2020-01-30 09:41:17 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,14 +3,28 @@ importScripts('./sw-toolbox.js');
 toolbox.precache([
     "./",
     "./index.html",
-    "./img/icons/**.*",
+    "./ar-library-navigation.html",
+    "./berita.html",
+    "./ebookejournal.html",
+    "./Navigasi.html",
+    "./opac.html",
+    "./openaccess.html",
+    "./peta.html",
+    "./pinjam.html",
+    "./UniUca.html",
     "./css/creative.css",
+    "./css/navigasi.css",
+    "./css/pinjam.css",    
+    "./dispatcher/**.*",
+    "./img/icons/**.*",
+    "./js/**.*",
+    "./lib/**.*",
+    "./scripts/**.*",
+    "./styles/inline.css",
     "./vendor/jquery/jquery.min.js",
     "https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js",
     "./vendor/scrollreveal/scrollreveal.min.js",
     "./endor/magnific-popup/jquery.magnific-popup.min.js",
-    "./js/creative.min.js",
-    "./scripts/app.js",
     "./vendor/bootstrap/css/bootstrap.min.css",
     "./vendor/font-awesome/css/font-awesome.min.css",
     "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800",
@@ -18,4 +32,6 @@ toolbox.precache([
     "./vendor/magnific-popup/magnific-popup.css"
 ]);
 toolbox.router.get('./img/*', toolbox.cacheFirst);
-toolbox.router.get('./*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+toolbox.router.get('./*', toolbox.networkFirst, {
+    networkTimeoutSeconds: 5
+});
