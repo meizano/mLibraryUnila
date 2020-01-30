203f2564768efa3e0643d8f892d9029a5da657ed
Mudipra <mudipra24@gmail.com>
2020-01-30 09:32:00 +0700

    Add files via upload

--- dispatcher/dbconfig.php
+++ dispatcher/dbconfig.php
@@ -1,16 +1,9 @@
 <?php
     # dbconfig.php
 
-//     # konfigurasi database localhost
-//     $host = 'localhost';
-//     $dbname = 'perpus';
-//     $username = 'root';
-//     $password = '';
-
-    # konfigurasi database elib
-    $host = '10.10.100.6';
-    $dbname = 'elib';
-    $username = 'aldi';
-    $password = 'aldi2019sql';
-
+    # konfigurasi database
+    $host = 'localhost';
+    $dbname = 'perpus';
+    $username = 'root';
+    $password = '';
 ?>
