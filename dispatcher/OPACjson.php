89593a24d83d1edbd085ec91ff5d5d87dae73374
Meizano A.M. Djausal <meizano@users.noreply.github.com>
2020-11-04 07:23:01 +0700

    Use rawurlencode() to change space to %20

--- dispatcher/OPACjson.php
+++ dispatcher/OPACjson.php
@@ -1,22 +1,43 @@
 <?php
 # Jika tidak ada parameter GET yang dikirimkan, tidak ada respon yang diberikan
-if($_GET){
+if ($_GET) {
     # Jika input type tidak ada, diberi nilai NULL
     $keywords = isset($_GET['keywords']) ? $_GET['keywords'] : NULL;
 
     ## BAGIAN 1 MENGOLAH HTML MENJADI OBYEK PHP ##
     # Mengambil HTML
-     $url = "http://opac.unila.ac.id/index.php?search=search&keywords=" . $keywords;
-//    $url = "http://localhost/git/mLibraryUnila/dispatcher/OPACjamur.html";
+    $url = "http://opac.unila.ac.id/index.php?search=search&keywords=" . rawurlencode($keywords);
+    //    $url = "http://localhost/git/mLibraryUnila/dispatcher/OPACjamur.html";
 
-    # Use the Curl extension to query and get back a page of results
-    $ch = curl_init();
-    $timeout = 5;
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-    $html = curl_exec($ch);
-    curl_close($ch);
+    function curl_get_contents($targeturl)
+    {
+
+        $timeout = 5;
+        // $cookie = tempnam("/tmp", "CURLCOOKIE");
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1");
+        curl_setopt($ch, CURLOPT_URL, $targeturl);
+        // curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_ENCODING, "");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);    # required for https urls
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        return $data;
+    }
+
+    # Query and get back a page of results
+    # file_get_contents() require php.ini to have line: allow_url_fopen = On
+    // $html = file_get_contents($url);
+    $html = curl_get_contents($url);
 
     # Create a DOM parser object
     $dom = new DOMDocument();
@@ -38,18 +59,10 @@ if($_GET){
     $infoJumlah = $infoJumlahLengkap;
     $infoJumlahValue = $infoJumlah->item(0)->nodeValue;
     // echo $infoJumlahValue;
-    # Informasi kata kunci pencarian
-    $infoKataKunci = $xpath->query("//span[@class='search-keyword-info']");
-    $infoKataKunciValue = $infoKataKunci->item(0)->nodeValue;
-    // echo $infoKataKunciValue;
-    # Informasi kata kunci pencarian
-    $infoWaktu = $xpath->query("//div[@class='search-query-time']");
-    $infoWaktuValue = $infoWaktu->item(0)->nodeValue;
-    // echo $infoWaktuValue;
 
     # Himpunan/kumpulan daftar pustaka yang ditemukan berdasarkan <div class="item">
-    $nlist = $xpath->query("//div[@class='item']"); 
-    
+    $nlist = $xpath->query("//div[@class='item']");
+
     # Mendapatkan link/tautan halaman berikutnya berdasarkan <a class="next_link">
     $nextLink = $xpath->query("//a[@class='next_link']");
 
@@ -57,7 +70,7 @@ if($_GET){
     $nLink = NULL;
     # Jika lebih dari satu halaman, maka dibuat link halaman berikut
     # ditambahkan absolute URL karena tautan yang tersimpan masih menggunakan relative URL
-    if($nextLink->item(0) != NULL)
+    if ($nextLink->item(0) != NULL)
         $nLink = 'http://opac.unila.ac.id' . $nextLink->item(0)->attributes->item(0)->nodeValue;
 
     ## AKHIR BAGIAN 1 MENGOLAH HTML MENJADI OBYEK PHP ##
@@ -68,10 +81,10 @@ if($_GET){
     $respon =  new stdClass();
 
     # Memberikan label, Sebagai penanda bahwa ini adalah data dari opac.unila.ac.id
-    $respon->label="opac.unila.ac.id";
-    
+    $respon->label = "opac.unila.ac.id";
+
     # Memasukkan informasi jumlah hasil pencarian ke dalam obyek $respon
-    $respon->info=$infoJumlahValue;
+    $respon->info = $infoJumlahValue;
 
     # Penampung nilai yang akan dimasukkan ke dalam obyek $respon
     $no = 0; // sebagai nomor urut
@@ -82,56 +95,55 @@ if($_GET){
     $cantuman = ''; // tautan menuju rincian pustaka
 
     # Menjadikan kumpulan elemen pada $nslist menjadi sebuah obyek $item (yang mewakili satu buah daftar pustaka)
-    foreach($nlist as $item) {
+    foreach ($nlist as $item) {
         # Menjadikan setiap elemen dom di dalam obyek $item sebagai $ite
-        foreach($item->childNodes as $ite) {
+        foreach ($item->childNodes as $ite) {
             # Menjadikan setiap elemen dom di dalam obyek $ite sebagai $it
-            foreach($ite->childNodes as $it) {
+            foreach ($ite->childNodes as $it) {
                 # jika ada $it yang memiliki tag html h4, dimasukkan sebagai nilai $judul
-                if($it->nodeName == 'h4') {
+                if ($it->nodeName == 'h4') {
                     $judul = $it->nodeValue;
                     continue;
                 }
                 # jika ada tag pada $it yang memiliki atribut, diperiksa disini
                 # Agar tidak muncul warning https://stackoverflow.com/questions/2385834/php-dom-get-all-attributes-of-a-node
                 if ($it->hasAttributes()) {
-                    foreach($it->attributes as $i) {
+                    foreach ($it->attributes as $i) {
                         # jika ada yang memiliki atribut 'author', dimasukkan sebagai nilai $pengarang
-                        if($i->nodeValue == 'author') {
-                            $pengarang = $it->nodeValue;
-                        } 
+                        if ($i->nodeValue == 'author') {
+                            $pengarang = substr($it->nodeValue, 12);
+                        }
                         # jika ada yang memiliki atribut 'customField callNumberField', dimasukkan sebagai nilai $callNumber
-                        else if($i->nodeValue == 'customField callNumberField') {
-                            $callNumber = $it->nodeValue;
-                        } 
+                        else if ($i->nodeValue == 'customField callNumberField') {
+                            $callNumber = substr($it->nodeValue, 14, 3);
+                        }
                         # jika ada yang memiliki atribut 'customField availabilityField', dimasukkan sebagai nilai $tersedia
-                        else if($i->nodeValue == 'customField availabilityField') {
-                            $tersedia = $it->nodeValue;
-                        } 
+                        else if ($i->nodeValue == 'customField availabilityField') {
+                            $str = $it->nodeValue;
+                            $tersedia = (int) filter_var($str, FILTER_SANITIZE_NUMBER_INT);
+                        }
                         # jika ada yang memiliki atribut 'subItem', dimasukkan sebagai nilai $cantuman
                         # ditambahkan absolute URL karena tautan yang tersimpan masih menggunakan relative URL
-                        else if($i->nodeValue == 'subItem') {
+                        else if ($i->nodeValue == 'subItem') {
                             $cantuman = 'http://opac.unila.ac.id' . $it->childNodes->item(0)->attributes->item(0)->nodeValue;
                         }
                     }
                 }
-
             }
         }
 
         # Menambahkan data yang dikumpulkan dari $item dan menyusunnya sesuai keperluan ke dalam key data
         # $no digunakan sebagai indeks dari data
-        $respon->data[$no]=array($judul, $pengarang, $callNumber, $tersedia, $cantuman);//ambil yang perlu saja
-        
+        $respon->data[$no] = array($judul, $pengarang, $callNumber, $tersedia, $cantuman); //ambil yang perlu saja
+
         # setelah data satuan ditambahkan ke key data, indeks ditambahkan satu
-        $no+=1;
+        $no += 1;
     }
 
     # Menambahkan informasi link/tautan ke halaman berikut
-    $respon->nextLink=$nLink;
+    $respon->nextLink = $nLink;
 
     echo json_encode($respon);
     /* JSON */
     ## AKHIR BAGIAN 2 MEMBUAT RESPON KELUARAN JSON DARI OBYEK PHP ##
 }
-?>
